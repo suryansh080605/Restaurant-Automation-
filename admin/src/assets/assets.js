b2bd2aa5888d2ee30fa86c4402a0e87bb5626367
suryansh080605 <ysuryansh885@gmail.com>
2026-08-16 20:38:29 +0530

    Add files via upload

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,18 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+//idhar maine sari images import karke ek object whom i have named assets kit arah export kar rha hu
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
+
+export const url = 'https://foodydesk-backend.onrender.com'
